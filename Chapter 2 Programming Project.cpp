0e757877237aa1ad9c1164c2ca4ab8671c16d754
kylebell7799@gmail.com <kylebell7799@gmail.com>
2024-09-19 12:56:52 -0400

    1st problem of the CH.2 programming project. Ran into some issues, wondering why the variable names were being difficult, then remembered they cant start with integers

--- Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project.cpp
@@ -1,20 +1,34 @@
 // Chapter 2 Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
+// Kyle Bell
+// CSC221
+
 
 #include <iostream>
+#include <string>    // has to be used for the string class in c++
+
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string fiveYears, sevenYears, tenYears; // String declaration
+
+// assigning the value/ initialize the variables 
+
+        fiveYears = "7.5";
+
+        sevenYears = "10.5";
+
+        tenYears = "15.0";
+
+// cout statements following the assignment instructions
+
+    cout << "In 5 years the sea level will have risen " << fiveYears << "mm." << endl;
+
+    cout << "In 7 years the sea level will have risen " << sevenYears << "mm." << endl;
+
+    cout << "In 10 years the sea level will have risen " << tenYears << "mm." << endl;
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
